452fa117e1182703edc5e0ea30f07d92859ff17c
Darkhan Akmyrza <darkhan_akmyrza@epam.com>
2021-07-15 11:34:31 +0500

    first commit

---
+++ src/main/java/com/example/primesourcetodo/PrimeSourceToDoApplication.java
@@ -0,0 +1,13 @@
+package com.example.primesourcetodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeSourceToDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeSourceToDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/primesourcetodo/PrimeSourceToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primesourcetodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeSourceToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
